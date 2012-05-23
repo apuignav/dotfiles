a9448c67548ae412fcf90d9f39b3c2cc789a3d43
Albert Puig <apuig@ecm.ub.edu>
2012-05-23 11:57:52 -0400

    Reorganized multiple OSs.
    Also rmoved 140 char limit in vim

---
+++ config/deploy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+here = os.path.dirname( __file__ )
+
+def getSystemType():
+  return os.uname()[0]
+
+
+for link, target in ( ( ".aliases", os.path.join( here, "aliases_" + getSystemType()  ) ), ( ".paths", os.path.join( here, "paths_" +
+  getSystemType()) ) ):
+  target = os.path.realpath( target )
+  link = os.path.expanduser( os.path.join( "~", link ) ) 
+  if os.path.exists( link ):
+    if os.path.islink( link ):
+      if os.readlink( link ) == target:
+        continue
+    if os.path.isdir( link ):
+      shutil.rmtree( link )
+    else:
+      os.unlink( link )
+  print "Linking %s -> %s" % ( link, target )
+  os.symlink( target, link )
+
+for link, target in ( ( ".latex.mk", os.path.join( here, "latex.mk" ) ), ( ".functions", os.path.join( here, "functions" ) ) ):
+  target = os.path.realpath( target )
+  link = os.path.expanduser( os.path.join( "~", link ) ) 
+  if os.path.exists( link ):
+    if os.path.islink( link ):
+      if os.readlink( link ) == target:
+        continue
+    if os.path.isdir( link ):
+      shutil.rmtree( link )
+    else:
+      os.unlink( link )
+  print "Linking %s -> %s" % ( link, target )
+  os.symlink( target, link )
+
