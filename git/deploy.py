79e148bf74d070cb7afd5e099f54734e7485f649
Albert Puig <albert.puig@epfl.ch>
2012-08-28 17:46:10 +0200

    Added general configuration for git, to be run once

---
+++ git/deploy.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import os
+
+os.system( "git config --global color.ui auto" )
+os.system( 'git config --global author.name "Albert Puig"' )
+os.system( 'git config --global author.email "apuignav@cern.ch"' )
