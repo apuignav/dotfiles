a5e1bd4b83c3114c01eee3a8c1482a36cb5bdde6
Albert Puig <albert.puig@epfl.ch>
2012-10-08 14:44:10 +0200

    Added scripts dir

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,7 +4,7 @@ import os
 import shutil
 here = os.path.dirname( __file__ )
 
-for link, target in ( ( ".vimrc", os.path.join( here, "vimrc" ) ), ( ".vim", os.path.join( here, "vim" ) ) ):
+for link, target in [ ( ".scripts", os.path.join( here, "bin" ) ) ]:
   target = os.path.realpath( target )
   link = os.path.expanduser( os.path.join( "~", link ) )
   if os.path.exists( link ):
@@ -18,10 +18,3 @@ for link, target in ( ( ".vimrc", os.path.join( here, "vimrc" ) ), ( ".vim", os.
   print "Linking %s -> %s" % ( link, target )
   os.symlink( target, link )
 
-for dName in ( "backups", "swaps", "undo" ):
-  dName = os.path.expanduser( os.path.join( "~", ".vim", dName ) )
-  if not os.path.isdir( dName ):
-    os.makedirs( dName )
-
-os.system("git clone http://github.com/gmarik/vundle.git $HOME/dotfiles/vim/vim/bundle/vundle")
-os.system( "vim +BundleInstall +qall" )
