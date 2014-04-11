56e35d21c3ec0425327c8884ed02613ac8e2716d
Albert Puig <albert.puig@cern.ch>
2014-04-12 00:34:01 +0200

    Small fixes in deploy.py

--- config/deploy.py
+++ config/deploy.py
@@ -5,29 +5,29 @@ import shutil
 here = os.path.dirname( __file__ )
 
 def getSystemType():
-  return os.uname()[0]
+    return os.uname()[0]
 
 things_to_link = ( ( ".aliases", os.path.join( here, "aliases_" + getSystemType()  ) ),
                    ( ".paths", os.path.join( here, "paths_" + getSystemType()) ),
                    ( ".common", os.path.join( here, "common" ) ),
                    ( ".functions", os.path.join( here, "functions" ) ),
-                   ( ".latex.mk", os.path.join( here, "latex.mk" ) ),
+                   #( ".latex.mk", os.path.join( here, "latex.mk" ) ),
                  )
 
 for link, target in things_to_link:
-  target = os.path.realpath( target )
-  link = os.path.expanduser( os.path.join( "~", link ) )
-  if os.path.islink( link ):
-    if os.readlink( link ) == target:
-      continue
-    else:
-      os.unlink( link )
-  if os.path.isdir( link ):
-    shutil.rmtree( link )
-  elif os.path.exists( link ):
-    os.unlink( link )
-  print "Linking %s -> %s" % ( link, target )
-  os.symlink( target, link )
+    target = os.path.realpath( target )
+    link = os.path.expanduser( os.path.join( "~", link ) )
+    if os.path.islink( link ):
+        if os.readlink( link ) == target:
+            continue
+        else:
+            os.unlink( link )
+    if os.path.isdir( link ):
+        shutil.rmtree( link )
+    elif os.path.exists( link ):
+        os.unlink( link )
+    print "Linking %s -> %s" % ( link, target )
+    os.symlink( target, link )
 
 if getSystemType() == 'Darwin':
     os.system("defaults write com.apple.Dock showhidden -bool YES && killall Dock")
--- mac/deploy.py
+++ mac/deploy.py
@@ -6,7 +6,6 @@
 import os
 
 def which(program):
-    import os
     def is_exe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
