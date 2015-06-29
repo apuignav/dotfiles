2d9ec42a9dac62c07362e155851c2a9574c7dbf1
Albert Puig <albert.puig@cern.ch>
2015-06-30 00:05:19 +0200

    Updated prezto deploy

--- zsh/deploy_prezto.sh
+++ zsh/deploy_prezto.sh
@@ -3,6 +3,11 @@
 if [ ! -d "$HOME/.prezto" ]; then
     GIT_SSL_NO_VERIFY=true
     git clone --recursive https://github.com/sorin-ionescu/prezto.git $HOME/.prezto
+else
+    olddir=$PWD
+    cd $HOME/.prezto
+    git pull && git submodule update --init --recursive
+    cd $olddir
 fi
 
 # EOF
