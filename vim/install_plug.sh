f80390e8edada3bb8c7ce513f009ed248b8de515
Albert Puig <albert.puig@cern.ch>
2015-06-30 09:56:58 +0200

    Update plug install script

--- vim/install_plug.sh
+++ vim/install_plug.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install plug if not there
+if [ ! -e ~/.vim/autoload/plug.vim ]; then
+    echo "Hi"
+    # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Install and upgrade  plugins
 if [ ! -d "$HOME/.vim/plugged" ]; then
     vim +PlugInstall
 else
-    vim +PlugUpdate
+    vim +PlugUpgrade +PlugInstall +PlugUpdate +q +q
 fi
 
 # EOF
