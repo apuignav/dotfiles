30db83765ef1d738516ab95af1ca42e763e1227f
Albert Puig <albert.puig@cern.ch>
2015-06-30 10:28:23 +0200

    Improved ssh scripts

--- ssh/fix_permissions.sh
+++ ssh/fix_permissions.sh
@@ -3,6 +3,8 @@
 chmod 700 ~/.ssh
 chmod 644 ~/.ssh/*
 chmod 600 ~/.ssh/id_dsa
-chmod 600 ~/.ssh/authorized_keys
+if [[ -e ~/.ssh/authorized_keys ]]; then
+    chmod 600 ~/.ssh/authorized_keys
+fi
 
 # EOF
--- ssh/setup_keys.sh
+++ ssh/setup_keys.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ ! -f "$HOME/.ssh/id_dsa" ]; then
-    cd $TMPDIR
+    mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+    cwd=$PWD
+    cd $mytmpdir
     wget -O keys.zip.gpg "https://www.dropbox.com/s/y11x0abb2uwpu8c/keys.zip.gpg?dl=0"
     gpg -d keys.zip.gpg > keys.zip
     unzip keys.zip
@@ -11,4 +13,6 @@ if [ ! -f "$HOME/.ssh/id_dsa" ]; then
     fi
     cp albert/id_dsa $HOME/.ssh/id_dsa
     cp albert/id_dsa.pub $HOME/.ssh/id_dsa.pub
+    cd $cwd
+    rm -rf $mytmpdir
 fi
