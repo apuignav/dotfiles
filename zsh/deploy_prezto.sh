75b46be7d88d00a075ff9ac5beb8c1b7e6d9dce2
Albert Puig <albert.puig@cern.ch>
2017-03-19 00:52:36 +0100

    Bugfix in prezto folder name

--- zsh/deploy_prezto.sh
+++ zsh/deploy_prezto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.prezto" ]; then
+if [ ! -d "$HOME/.zprezto" ]; then
     GIT_SSL_NO_VERIFY=true
-    git clone --recursive https://github.com/sorin-ionescu/prezto.git $HOME/.prezto
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git $HOME/.zprezto
 else
     olddir=$PWD
-    cd $HOME/.prezto
+    cd $HOME/.zprezto
     git pull && git submodule update --init --recursive
     cd $olddir
 fi
