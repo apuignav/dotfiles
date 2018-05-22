c627c7e9e1cf8424d13f9bf75ef7a647d542002a
Albert Puig <albert.puig@cern.ch>
2018-05-22 15:37:51 +0200

    Add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $HOME
+git clone https://github.com/apuignav/dotfiles.git
+cd dotfiles
+git submodule update --init --recursive deploy/dotbot
+git submodule update --init --recursive deploy/dotbot-brew
+echo "Ready to install"
