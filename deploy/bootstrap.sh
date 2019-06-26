c3948d0bc485f469d1f49ca877977d413e2094e5
Albert Puig <albert.puig@cern.ch>
2019-06-26 22:46:35 +0200

    Updated bootstrap install script

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -4,4 +4,5 @@ cd $HOME
 git clone https://github.com/apuignav/dotfiles.git
 cd dotfiles
 git submodule update --init --recursive dotbot
+git submodule update --init --recursive deploy/dotbot-brew
 echo "Ready to install"
