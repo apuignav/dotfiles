9f717f1dc07af1b717d8cc348caad313920afc67
Albert Puig <albert.puig@cern.ch>
2019-06-27 08:38:20 +0200

    Fix typo

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -3,6 +3,6 @@
 cd $HOME
 git clone https://github.com/apuignav/dotfiles.git
 cd dotfiles
-git submodule update --init --recursive dotbot
+git submodule update --init --recursive deploy/dotbot
 git submodule update --init --recursive deploy/dotbot-brew
 echo "Ready to install"
